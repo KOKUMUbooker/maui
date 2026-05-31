5ab8323ec73d24de3fc9f5861ec66844fe4a71d4
KOKUMUbooker <bookerochieng20@gmail.com>
2026-05-31 21:26:12 +0300

    Implement static resource in XAML to hold uniform app font & bg colors

---
+++ 2.NotesApp/exercise3/Notes/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿namespace Notes;
+
+public partial class MainPage : ContentPage
+{
+    readonly string _fileName = Path.Combine(FileSystem.AppDataDirectory, "notes.txt");
+
+    public MainPage()
+    {
+        InitializeComponent();
+
+        if (File.Exists(_fileName))
+        {
+            editor.Text = File.ReadAllText(_fileName);
+        }
+    }
+
+    void OnSaveButtonClicked(object sender, EventArgs e)
+    {
+        File.WriteAllText(_fileName, editor.Text);
+    }
+
+    void OnDeleteButtonClicked(object sender, EventArgs e)
+    {
+        if (File.Exists(_fileName))
+        {
+            File.Delete(_fileName);
+        }
+        editor.Text = string.Empty;
+    }
+}
+
+// How to write OS specific code using C# 
+// MyStackLayout.Padding = 
+//    DeviceInfo.Platform == DevicePlatform.iOS
+//        ? new Thickness(30, 60, 30, 30) // Shift down by 60 points on iOS only
+//        : new Thickness(30); // Set the default margin to be 30 poi
