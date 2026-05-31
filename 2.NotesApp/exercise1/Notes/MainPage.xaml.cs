fb7b7f27dae96fa87927e43cb981f090d9e4ae92
KOKUMUbooker <bookerochieng20@gmail.com>
2026-05-31 17:19:27 +0300

    Convert from C# UI declaration to xaml in exercise 1

---
+++ 2.NotesApp/exercise1/Notes/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿namespace Notes;
+
+public partial class MainPage : ContentPage
+{
+    readonly string _fileName = Path.Combine(FileSystem.AppDataDirectory, "notes.txt");
+
+    public MainPage()
+    {
+        InitializeComponent();
+        if (File.Exists(_fileName))
+            NotesEditor.Text = File.ReadAllText(_fileName);
+    }
+
+    void OnSaveButtonClicked(object sender, EventArgs e)
+    {
+        File.WriteAllText(_fileName, NotesEditor.Text);
+    }
+
+    void OnDeleteButtonClicked(object sender, EventArgs e)
+    {
+        if (File.Exists(_fileName))
+        {
+            File.Delete(_fileName);
+        }
+        NotesEditor.Text = string.Empty;
+    }
+}
+
